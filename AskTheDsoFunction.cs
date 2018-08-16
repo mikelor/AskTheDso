6b4c446a4dfab049c11ad813fdf4cf0f7db350b4
mike lorengo <mikelor@gmail.com>
2018-08-15 21:26:16 -0700

    Minimal Alexa Skill

--- AskTheDsoFunction.cs
+++ AskTheDsoFunction.cs
@@ -8,24 +8,25 @@ using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
 
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+
 namespace AlaskaAir.AskTheDso
 {
     public static class AskTheDsoFunction
     {
         [FunctionName("AskTheDsoFunction")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]SkillRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            if (req.Request is LaunchRequest)
+            {
+                return new OkObjectResult(ResponseBuilder.Tell("you launched from method param"));
+            }
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkObjectResult(ResponseBuilder.Empty());;
         }
     }
 }
